3ae5b953284e0e53dd5a122c5bf0f02a3dc6f33a
Rodolfo Neto <rodolfotiago@gmail.com>
2019-06-07 18:42:48 -0300

    Update odroid-bot.tf

--- Terraform/odroid-bot.tf
+++ Terraform/odroid-bot.tf
@@ -38,6 +38,7 @@ resource "google_compute_instance" "default" {
       type    = "ssh"
       user    = "root"
       timeout = "120s"
+      host = "${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}"
     }
 
     inline = [
