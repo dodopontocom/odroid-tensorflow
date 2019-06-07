dc2c151d8b164bc4cbe1b8d52f93b9607ab1dcd3
Rodolfo Neto <rodolfotiago@gmail.com>
2019-06-07 18:18:18 -0300

    Create ssh-key.tf

---
+++ Terraform/ssh-key.tf
@@ -0,0 +1,11 @@
+resource "null_resource" "generate-sshkey" {
+    provisioner "local-exec" {
+        command = "yes y | ssh-keygen -b 4096 -t rsa -C 'terraform' -N '' -f ${var.virtual_machine_kubernetes_controller.["private_key"]}"
+    }
+}
+
+resource "null_resource" "ssh-keygen-delete" {
+    provisioner "local-exec" {
+        command = "ssh-keygen -R ${var.virtual_machine_kubernetes_controller.["ip_address"]}"
+    }
+}
