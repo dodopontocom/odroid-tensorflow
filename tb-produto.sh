f9e86c7ac876fba6124933877b7e8c579b1d97c1
Rodolfo Neto <rodolfotiago@gmail.com>
2019-06-07 14:26:02 -0300

    Update tb-produto.sh

--- tb-produto.sh
+++ tb-produto.sh
@@ -42,7 +42,7 @@ do
 						--text "Processando, aguarde uns instantes..." \
 						--parse_mode markdown
 
-					message=$(docker run --rm -i -v ${PWD}:/home/tensor-photos tensorflow python /home/tensor-example.py "/home/tensor-photos/$(echo $file_path | cut -d'|' -f2 | sed 's#\.\/##')" > /tmp/usar_urandom.log)
+					message=$(sudo docker run --rm -i -v ${PWD}:/home/tensor-photos tensorflow python /home/tensor-example.py "/home/tensor-photos/$(echo $file_path | cut -d'|' -f2 | sed 's#\.\/##')" > /tmp/usar_urandom.log)
 					produto=$(tail -2 /tmp/usar_urandom.log | head -1)
 					if [[ $? -eq 0 ]]; then
 						elapsed="\`(tempo de processamento: $(tail -1 /tmp/usar_urandom.log) segundos)\`\n"
