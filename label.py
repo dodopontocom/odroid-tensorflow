5604d40215a5d43ea2eabeb4dcc4a71fb4c15ae0
rodolfo <rodolfotiago@gmail.com>
2019-06-09 23:03:34 -0300

    adicionando o tempo de execucao

--- label.py
+++ label.py
@@ -9,14 +9,14 @@ image_path = sys.argv[1]
 image_data = tf.gfile.FastGFile(image_path, 'rb').read()
 
 label_lines = [line.rstrip() for line
-        in tf.gfile.GFile("/home/tensor-photos/retrained_labels.txt")]
+        in tf.io.gfile.GFile("/home/tensor-photos/retrained_labels.txt")]
 
 with tf.gfile.FastGFile("/home/tensor-photos/retrained_graph.pb", 'rb') as f:
-        graph_def = tf.GraphDef()
+        graph_def = tf.compat.v1.GraphDef()
         graph_def.ParseFromString(f.read())
         _ = tf.import_graph_def(graph_def, name='')
 
-with tf.Session() as sess:
+with tf.compat.v1.Session() as sess:
         softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
         predictions = sess.run(softmax_tensor, {'DecodeJpeg/contents:0': image_data})
         #predictions = sess.run(softmax_tensor, {'Placeholder:0': image_data})
