7e27a7aa670a66e6b510c2937f38ed59679bc6ac
rodolfotiago <rodolfotiago@gmail.com>
2019-06-11 14:07:35 -0300

    testing

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -2,10 +2,4 @@ provider "google" {
   credentials = "${file("~/account.json")}"
   project     = "odroid-tensorflow-243011"
   region      = "us-central1"
-
-  /*provisioner "file" {
-      source = "../account.json"
-      destination = "/tmp/account.json"
-  }*/
- 
 }
