e35344b010d4d4c4ffa98c99b5dcbafacd51ff56
rodolfo <rodolfotiago@gmail.com>
2019-06-10 21:24:22 -0300

    terraform again

--- Terraform/odroid-bot.tf
+++ Terraform/odroid-bot.tf
@@ -14,7 +14,32 @@ resource "google_compute_instance" "default" {
   }
 
   metadata_startup_script = <<EOH
-  /bin/bash ../_scripts/deploy-gcp.sh  
+sudo apt-get update
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common \
+	jq
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker $USER
+
+git clone https://github.com/dodopontocom/odroid-tensorflow.git
+PROJECT=~/odroid-tensorflow
+TB_TOKEN=<TELEGRAM_TOKEN>
+echo $TB_TOKEN > $PROJECT/.token
+cd $PROJECT
+sudo docker build -t tensorflow . && sudo bash ./tb-produto.sh
   EOH
 
   network_interface {
