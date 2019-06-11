2a5d071c2f5ae73fc46da9ec3287f3132882a788
rodolfo <rodolfotiago@gmail.com>
2019-06-10 23:15:37 -0300

    comando para deployar com gcloud pois terraform nao ta indo

--- gcp-compute-creation-command.sh
+++ gcp-compute-creation-command.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 name=$1
 if [[ -n $name ]]; then
-    gcloud compute instances create $name --zone us-central1-a --machine-type f1-micro --image-family ubuntu-1804-lts --tags http-server --metadata-from-file startup-script='../_scripts/deploy-gcp.sh'
+    gcloud compute instances create $name --zone us-central1-a --machine-type f1-micro --image-family ubuntu-1804-lts --tags http-server --metadata-from-file startup-script='_scripts/deploy-gcp.sh'
 fi
