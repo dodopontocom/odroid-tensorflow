a29f46f6000b4f57208c455ee6ba5d086f297e39
rodolfo <rodolfotiago@gmail.com>
2019-06-10 22:40:56 -0300

    terraform again

--- Terraform/odroid-bot.tf
+++ Terraform/odroid-bot.tf
@@ -9,7 +9,8 @@ resource "google_compute_instance" "default" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      //image = "debian-cloud/debian-9"
+      image = "ubuntu-os-cloud/ubuntu-1804-lts"
     }
   }
   //metadata_startup_script = "${file("./startup.ign")}"
