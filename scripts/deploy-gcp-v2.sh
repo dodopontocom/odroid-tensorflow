db769bfeaab3c0648704a7a19b59d1a821dd52b9
Rodolfo Neto <rodolfotiago@gmail.com>
2019-06-12 15:50:45 -0300

    Update deploy-gcp-v2.sh
    [skip ci]

--- scripts/deploy-gcp-v2.sh
+++ scripts/deploy-gcp-v2.sh
@@ -11,6 +11,8 @@ curl -s -X POST $URL -d chat_id=$CHAT_ID -d text="$init_msg"
 apt-get update
 apt-get install -y docker.io jq curl git
 
+docker pull rodolfoneto/tensorflow-retrained-experience
+
 git clone -b develop https://github.com/dodopontocom/odroid-tensorflow.git
 curl -s -X POST $URL -d chat_id=$CHAT_ID -d text="$MESSAGE"
 bash ./odroid-tensorflow/scripts/tb-produto.sh $TOKEN > /tmp/tb-produto-bot.log 2>&1
